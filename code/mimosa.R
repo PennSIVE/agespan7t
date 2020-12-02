d37a175bad129baed03d875150435e6cffb4cc18
Tim <tim.terf@gmail.com>
2020-12-02 10:23:27 -0500

    small bug

--- code/mimosa.R
+++ code/mimosa.R
@@ -2,11 +2,11 @@ library(fslr)
 library(mimosa)
 
 index = as.numeric(Sys.getenv("INDEX"))
-train_dir = "/cbica/projects/agespan7T/data/scitran/cnet/7T-MS-agespan"
+train_dir = "../data/scitran/cnet/7T-MS-agespan"
 subjs = c('7TAS-003', '7TAS-004', '7TAS-007', '7TAS-010', '7TAS-011', '7TAS-013', '7TAS-017', '7TAS-018', '7TAS-024')
 t1_files = list.files(path = file.path(train_dir, subjs), pattern = "^UNI_ws.nii.gz$", full.names = TRUE, recursive = TRUE)
 flair_files = list.files(path = file.path(train_dir, subjs), pattern = "^T1map_ws.nii.gz$", full.names = TRUE, recursive = TRUE)
-tissue_files = list.files(path = file.path(train_dir, subjs), pattern = "^tissue_mask_11_25.nii.gz$", full.names = TRUE, recursive = TRUE)
+tissue_files = list.files(path = file.path(train_dir, subjs), pattern = "^tissue_mask.nii.gz$", full.names = TRUE, recursive = TRUE)
 brainmask_files = list.files(path = file.path(train_dir, subjs), pattern = "^UNI_binmask.nii.gz$", full.names = TRUE, recursive = TRUE)
 mask_files = list.files(path = file.path(train_dir, subjs), pattern = "mks-bin.nii.gz$", full.names = TRUE, recursive = TRUE)
 filepaths = data.frame(t1 = t1_files, flair = flair_files, mask = mask_files, brainmask = brainmask_files, tissue = tissue_files, stringsAsFactors = FALSE)
@@ -25,7 +25,7 @@ start = proc.time()
 # writenii(tissue_mask, paste0(train_dir, "/", subjs[index],
 #                                  "/tissue_mask_11_25.nii.gz"))
 
-tissues = c()
+tissues = character()
 i = 1
 for (path in filepaths$tissue[-index]) {
   tissues[i] = readnii(path)
